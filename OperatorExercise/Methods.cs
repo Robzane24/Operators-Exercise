d16ebb19985e62ba7bde5e1c8249addab44d93a4
Robert Zane <rob.zane24@gmail.com>
2022-11-23 14:41:56 -0600

    Completed Operators

---
+++ OperatorExercise/Methods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class Methods
+    {
+        public static double AreaOfCircle(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+    }
+}
--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace OperatorExercise
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise one
+
+            int a = 11;
+            int b = 2;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            //Exercise two
+            Console.WriteLine("Please enter the radius of your circle:");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
         }
     }
 }
